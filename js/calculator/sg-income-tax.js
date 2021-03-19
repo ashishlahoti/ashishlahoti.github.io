f90ed2c4ded0f7e9ec4812316cefda399ac531a5
Ashish Lahoti <lahoti.ashish20@gmail.com>
2021-03-19 20:14:11 +0800

    postupdate: calculate singapore tax

--- js/calculator/sg-income-tax.js
+++ js/calculator/sg-income-tax.js
@@ -7,19 +7,39 @@ const calculateIncomeTax = () => {
   const salaryPeriod = document.getElementById("salary-period").value ?? 0;
   const bonusPeriod = document.getElementById("bonus-period").value ?? 0;
   const taxSavingPeriod = document.getElementById("tax-saving-period").value ?? 0;
-  const totalSalary = Number(inputSalary*(salaryPeriod == 'monthly' ? 12 : 1)) + Number(inputBonus*(bonusPeriod == 'monthly' ? 12 : 1)) - Number(taxSaving*(taxSavingPeriod == 'monthly' ? 12 : 1));
+  const taxRelief = document.getElementById("tax-relief").value ?? 0;
+  let totalSalary = Number(inputSalary*(salaryPeriod == 'monthly' ? 12 : 1)) + Number(inputBonus*(bonusPeriod == 'monthly' ? 12 : 1)) - Number(taxSaving*(taxSavingPeriod == 'monthly' ? 12 : 1)) - taxRelief; 
   console.log("salary", inputSalary, salaryPeriod, "bonus", inputBonus, bonusPeriod, "saving", taxSaving, taxSavingPeriod, "total", totalSalary);
-  document.getElementById("taxable-income").value = totalSalary;
   let incomeTax = 0;
-  let salary = totalSalary;
-  Object.keys(lookup).map(key => Number(key)).sort((a, b) => b-a).forEach(slab => {
-    if(salary > slab){
-      let taxableIncome = Number(salary-slab);
-      let slabTax = Number(lookup[slab]);
-      incomeTax += taxableIncome*slabTax/100;
-      console.log(taxableIncome, slabTax, incomeTax);
-      salary = slab;
-    }
-  });
+  if(totalSalary <= 0){
+    totalSalary = 0;
+  }else{
+    let salary = totalSalary;
+    Object.keys(lookup).map(key => Number(key)).sort((a, b) => b-a).forEach(slab => {
+      if(salary > slab){
+        let taxableIncome = Number(salary-slab);
+        let slabTax = Number(lookup[slab]);
+        incomeTax += taxableIncome*slabTax/100;
+        console.log(taxableIncome, slabTax, incomeTax);
+        salary = slab;
+      }
+    });
+  }
+  document.getElementById("taxable-income").value = totalSalary;
   document.getElementById("income-tax").value = incomeTax;
+}
+
+const calculateTaxRelief = () => {
+  const inputAge = document.getElementById("age").value ?? 0;
+  let taxRelief = 0;
+  if(inputAge >= 1 && inputAge < 55){
+    taxRelief = 1000;
+  }else if(inputAge >= 55 && inputAge <=59){
+    taxRelief = 6000;
+  }else if(inputAge >= 60){
+    taxRelief = 8000;
+  }
+  console.log(taxRelief);
+  document.getElementById("tax-relief").value = taxRelief;
+  calculateIncomeTax();
 }
